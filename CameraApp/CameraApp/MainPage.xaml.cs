011b00a3695678dd8ba3cbec11cd2971497a1568
Adam <adamhomer88@gmail.com>
2014-01-07 08:17:26 -0800

    Added a camera App tool
    This camera app will contain methods/classes used in the Seeing Eye
    project.

---
+++ CameraApp/CameraApp/MainPage.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using CameraApp.Resources;
+using Windows.Phone.Media.Capture;
+using Microsoft.Xna.Framework.Media;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace CameraApp
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        PhotoCaptureDevice captureDevice;
+
+        public MainPage()
+        {
+            InitializeComponent();
+            captureDevice = await InitializeCamera();
+            
+        }
+
+        private async Task<PhotoCaptureDevice> InitializeCamera()
+        {
+            PhotoCaptureDevice PhotoCaptureDevice = null;
+
+            if (PhotoCaptureDevice.AvailableSensorLocations.Contains(CameraSensorLocation.Back))
+            {
+                System.Collections.Generic.IReadOnlyList<Windows.Foundation.Size> SupportedResolutions =
+                    PhotoCaptureDevice.GetAvailableCaptureResolutions(CameraSensorLocation.Back);
+                Windows.Foundation.Size res = SupportedResolutions[0];
+                PhotoCaptureDevice = await PhotoCaptureDevice.OpenAsync(CameraSensorLocation.Back, res);
+            }
+            return PhotoCaptureDevice;
+        }
+    }
+}
