6ea1593fff5ba3bf22a6a905455d676eed9693d5
Adam <adamhomer88@gmail.com>
2014-01-06 17:46:43 -0800

    Creating Seeing Eye Project Files

---
+++ SeeingEye/SeeingEye/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using SeeingEye.Resources;
+
+namespace SeeingEye
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
