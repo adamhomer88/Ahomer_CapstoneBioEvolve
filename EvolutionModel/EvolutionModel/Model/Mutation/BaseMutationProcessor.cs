cab7b9e8101db521c6f3d4d8e50bdd918da0b0fc
Adam <adamhomer88@gmail.com>
2014-02-03 10:34:34 -0700

    Changed BaseMutationProcessor to be singleton
    With the use of many dictionaries, it would be impractical to
    instantiate so many dictionaries everytime I need a mutation processor.
    So I setup my processors to be singletons.

--- EvolutionModel/EvolutionModel/Model/Mutation/BaseMutationProcessor.cs
+++ EvolutionModel/EvolutionModel/Model/Mutation/BaseMutationProcessor.cs
@@ -7,25 +7,29 @@ using System.Threading.Tasks;
 
 namespace EvolutionModel.Model.Mutation
 {
-    class BaseMutationProcessor : IMutationProcessor
+    public class BaseMutationProcessor : IMutationProcessor
     {
         private const int DEFAULT_BASE_MUTATION_MARGIN = 5;
+        private BaseMutationProcessor processor;
 
         public Organism MutateAnimal(Organism organism)
         {
             Animal mutatee = (Animal)mutateBaseOrganismPhenotypes(organism);
+            mutatee = mutateBaseAnimalPhenotypes(mutatee);
             return mutatee;
         }
 
         public Organism MutatePlant(Organism organism)
         {
             Plant mutatee = (Plant)mutateBaseOrganismPhenotypes(organism);
+            mutatee = mutateBasePlantPhenotypes(mutatee);
             return mutatee;
         }
 
         public Organism MutateParasite(Organism organism)
         {
             Parasite mutatee = (Parasite)mutateBaseOrganismPhenotypes(organism);
+            mutatee = mutateBaseParasitePhenotypes(mutatee);
             return mutatee;
         }
 
@@ -35,6 +39,36 @@ namespace EvolutionModel.Model.Mutation
             organism.MaxEnergy += OrganismFactory.random.Next(-(DEFAULT_BASE_MUTATION_MARGIN + 1), DEFAULT_BASE_MUTATION_MARGIN);
             organism.EnergyPerTurn += OrganismFactory.random.Next(-(DEFAULT_BASE_MUTATION_MARGIN + 1), DEFAULT_BASE_MUTATION_MARGIN);
             return organism;
+        }
+
+        private Plant mutateBasePlantPhenotypes(Plant mutatee)
+        {
+            mutatee.growthThresholdToNutrients += OrganismFactory.random.Next(-DEFAULT_BASE_MUTATION_MARGIN + 1, DEFAULT_BASE_MUTATION_MARGIN);
+            mutatee.growthRate += OrganismFactory.random.Next(-DEFAULT_BASE_MUTATION_MARGIN + 1, DEFAULT_BASE_MUTATION_MARGIN);
+            mutatee.MaxNutrient += OrganismFactory.random.Next(-DEFAULT_BASE_MUTATION_MARGIN + 1, DEFAULT_BASE_MUTATION_MARGIN);
+            mutatee.MaxWater += OrganismFactory.random.Next(-DEFAULT_BASE_MUTATION_MARGIN + 1, DEFAULT_BASE_MUTATION_MARGIN);
+            mutatee.ReproductionRate += OrganismFactory.random.Next(-DEFAULT_BASE_MUTATION_MARGIN + 1, DEFAULT_BASE_MUTATION_MARGIN);
+            return mutatee;
+        }
+
+        private Parasite mutateBaseParasitePhenotypes(Parasite mutatee)
+        {
+            return mutatee;
+        }
+
+        private Animal mutateBaseAnimalPhenotypes(Animal mutatee)
+        {
+            mutatee.favoredHungerThreshold += OrganismFactory.random.Next(-DEFAULT_BASE_MUTATION_MARGIN+1, DEFAULT_BASE_MUTATION_MARGIN);
+            mutatee.unfavoredHungerThreshold += OrganismFactory.random.Next(-DEFAULT_BASE_MUTATION_MARGIN + 1, DEFAULT_BASE_MUTATION_MARGIN);
+            mutatee.reproductionThreshold += OrganismFactory.random.Next(-DEFAULT_BASE_MUTATION_MARGIN + 1, DEFAULT_BASE_MUTATION_MARGIN);
+            return mutatee;
+        }
+
+        public BaseMutationProcessor getInstance()
+        {
+            if (this.processor == null)
+                this.processor = new BaseMutationProcessor();
+            return this.processor;
         }
     }
 }
---
+++ EvolutionModel/EvolutionModel/Model/Mutation/ComplexMutationProcessor.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EvolutionModel.Model.Mutation
+{
+    class ComplexMutationProcessor : IMutationProcessor
+    {
+        public Genotypes.Organism MutateAnimal(Genotypes.Organism organism)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Genotypes.Organism MutatePlant(Genotypes.Organism organism)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Genotypes.Organism MutateParasite(Genotypes.Organism organism)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
